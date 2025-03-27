37e58cce3b760cb29dd33e5cfc65f5bbfde4367c
Matt <matt@Matts-MacBook-Pro.local>
2025-03-27 15:31:48 -0400

    Cleaning up

--- scripts/deploy-gitea.sh
+++ scripts/deploy-gitea.sh
@@ -31,10 +31,15 @@ kubectl create namespace gitea || true
 echo "🏷️  Labeling node for Gitea..."
 kubectl label node ${GITEA_NODE_NAME} dedicated=gitea --overwrite
 
+echo "Importing Gitea Actions Token"
+kubectl apply -f "$SECRETS_PATH/gitea-actions-token-sealed-secret.yaml"
+echo "Gitea Actions Token Imported Successfully!"
+
 # Restore Persistent Volume from backup for Gitea
 echo "🔐 Restoring Data Volume..."
-base/scripts/longhorn-automation.sh restore gitea
-base/scripts/longhorn-automation.sh restore gitea-postgres-db --wrapper
+# base/scripts/longhorn-automation.sh restore gitea
+# base/scripts/longhorn-automation.sh restore gitea-postgres-db --wrapper
+# base/scripts/longhorn-automation.sh restore gitea-actions-docker --wrapper
 echo "✅ Persistent Data Volume Restored!"
 
 # Deploy Gitea
@@ -42,7 +47,7 @@ helm dependency update "$HELM_CHARTS_PATH/gitea"
 helm upgrade --install gitea "$HELM_CHARTS_PATH/gitea" \
   --namespace gitea \
   --values "$HELM_VALUES_PATH/gitea-values.yaml" \
-  --values "$HELM_VALUES_PATH/gitea-restored-volume.yaml" \
-  --values "$HELM_VALUES_PATH/gitea-postgres-db-restored-volume.yaml"
+  # --values "$HELM_VALUES_PATH/gitea-restored-volume.yaml" \
+  # --values "$HELM_VALUES_PATH/gitea-postgres-db-restored-volume.yaml"
 
 echo "✅ Gitea Deployed Successfully!"
